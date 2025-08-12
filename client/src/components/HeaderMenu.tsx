c4716704d6a407405b30d7e5ab5c15d68cd5c0df
ElliottMtz <elliottmartinez35@gmail.com>
2025-08-12 12:08:44 -0600

    Revamp header menu navigation and layout
    Updated the header menu to change main navigation links, introduce a configuration dropdown with additional dashboard routes, and add a logout button. The home link now points to '/floor' and the menu structure is more organized for improved usability.

--- client/src/components/HeaderMenu.tsx
+++ client/src/components/HeaderMenu.tsx
@@ -1,18 +1,24 @@
 import Link from 'next/link';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import LogOutButton from '@/components/LogoutButton';
 
 export default function MainMenu() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className='container-fluid'>
-                <Link className="navbar-brand" href="/protected/dashboard">
+                <Link className="navbar-brand" href="/floor">
                     <i className="bi bi-house-door-fill me-2"></i>
-                    Restaurant POS
+                    Restaurant
                 </Link>
 
-                <button className='navbar-toggler' type="button" data-bs-toggle="offcanvas" data-bs-target="#menuLateral">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+
+                {/* <div className="row w-100 text-right"> */}
+                    {/* <div className="col"> */}
+                        <button className='navbar-toggler' type="button" data-bs-toggle="offcanvas" data-bs-target="#menuLateral">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    {/* </div> */}
+                {/* </div> */}
 
                 <section className="offcanvas offcanvas-start bg-dark" id="menuLateral" tabIndex={-1}>
                     <div className="offcanvas-header" data-bs-theme="dark">
@@ -23,19 +29,40 @@ export default function MainMenu() {
                     <div className="offcanvas-body d-flex flex-column justify-content-between px-0">
                         <ul className="navbar-nav fs-5 justify-content-evenly">
                             <li className="nav-item p-3 py-md-1">
-                                <Link href="/protected/dashboard" className="nav-link">Inicio</Link>
+                                <Link href="/floor" className="nav-link">Piso de venta</Link>
                             </li>
-                            <li className="nav-item p-3 py-md-1">
-                                <Link href="/protected/tables" className="nav-link">Mesas</Link>
-                            </li>
-                            <li className="nav-item p-3 py-md-1">
-                                <Link href="/protected/users" className="nav-link">Usuarios</Link>
-                            </li>
-                            <li className="nav-item p-3 py-md-1">
-                                <Link href="/protected/products" className="nav-link">Productos</Link>
+                            <li className="nav-item dropdown p-3 py-md-1">
+                                {/* <Link href="/protected/tables" className="nav-link">Configuración</Link> */}
+                                <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Configuración</a>
+                                <ul className="dropdown-menu dropdown-menu-dark">
+                                    <li>
+                                        <Link href="/dashboard/tables" className="dropdown-item">Mesas</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/dashboard/products" className="dropdown-item">Productos</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/dashboard/categories" className="dropdown-item">Categorías</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/dashboard/users" className="dropdown-item">Usuarios</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/dashboard/inventory" className="dropdown-item">Bitacora de inventarios</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/dashboard/roles" className="dropdown-item">Roles</Link>
+                                    </li>
+
+                                </ul>
                             </li>
                             {/* Add more links as needed */}
                         </ul>
+
+                        <div className="d-long d-md-flex justify-content-center p-3">
+                            <LogOutButton />
+                        </div>
+
                     </div>
                 </section>
             </div>
